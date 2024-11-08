dcf73ecfb5d44e72ae341db39fe98622dff97810
Garrett Kinman <garrettkinman@gmail.com>
2024-11-08 16:10:22 -0500

    fixed comments

--- src/tinytensor/primops.nim
+++ src/tinytensor/primops.nim
@@ -55,50 +55,7 @@ func scale*[T; n: static int](a: StridedVector[T, n], b: T, result: var StridedV
     for i in 0..<n:
         result.data[i] = a.data[i] * b
 
-# func `+`*[T; shape: static TensorShape](A, B: Tensor[T, shape]): Tensor[T, shape] =
-#     result = initTensor[T, shape]()
-#     for i in 0..<result.data.len:
-#         result.data[i] = A.data[i] + B.data[i]
-
-# func `+`*[T; shape: static TensorShape](A: Tensor[T, shape], a: T): Tensor[T, shape] =
-#     result = initTensor[T, shape]()
-#     for i in 0..<result.data.len:
-#         result.data[i] = A.data[i] + a
-
-# func `+`*[T; shape: static TensorShape](a: T, A: Tensor[T, shape]): Tensor[T, shape] =
-#     result = initTensor[T, shape]()
-#     for i in 0..<result.data.len:
-#         result.data[i] = A.data[i] + a
-
-# func `-`*[T; shape: static TensorShape](A, B: Tensor[T, shape]): Tensor[T, shape] =
-#     result = initTensor[T, shape]()
-#     for i in 0..<result.data.len:
-#         result.data[i] = A.data[i] - B.data[i]
-
-# func `-`*[T; shape: static TensorShape](A: Tensor[T, shape], a: T): Tensor[T, shape] =
-#     result = initTensor[T, shape]()
-#     for i in 0..<result.data.len:
-#         result.data[i] = A.data[i] - a
-
-# func `-`*[T; shape: static TensorShape](A: Tensor[T, shape]): Tensor[T, shape] =
-#     result = initTensor[T, shape]()
-#     for i in 0..<result.data.len:
-#         result.data[i] = -A.data[i]
-
-# func hadamard*[T; shape: static TensorShape](A, B: Tensor[T, shape]): Tensor[T, shape] =
-#     result = initTensor[T, shape]()
-#     for i in 0..<result.data.len:
-#         result.data[i] = A.data[i] * B.data[i]
-
-# func `*`*[T; shape: static TensorShape](A: Tensor[T, shape], a: T): Tensor[T, shape] =
-#     result = initTensor[T, shape]()
-#     for i in 0..<result.data.len:
-#         result.data[i] = A.data[i] * a
-
-# func `*`*[T; shape: static TensorShape](a: T, A: Tensor[T, shape]): Tensor[T, shape] =
-#     result = initTensor[T, shape]()
-#     for i in 0..<result.data.len:
-#         result.data[i] = A.data[i] * a
+# TODO: dot product?
 
 # ~~~~~~~~~~~~~~~~~~~~
 # reduction operations
@@ -143,42 +100,6 @@ func argmin*[T; n: static int](a: StridedVector[T, n]): int =
             minVal = val
             result = i
 
-
-# func sum*[T; shape: static TensorShape](A: Tensor[T, shape]): T =
-#     for i in 0..<A.data.len:
-#         result += A.data[i]
-
-# func mean*[T; shape: static TensorShape](A: Tensor[T, shape]): T =
-#     result = A.sum() * (1 / A.data.len)
-
-# func max*[T; shape: static TensorShape](A: Tensor[T, shape]): T =
-#     result = A.data[0]  # Initialize with first element
-#     for i in 1..<A.data.len:
-#         if A.data[i] > result:
-#             result = A.data[i]
-
-# func min*[T; shape: static TensorShape](A: Tensor[T, shape]): T =
-#     result = A.data[0]  # Initialize with first element
-#     for i in 1..<A.data.len:
-#         if A.data[i] < result:
-#             result = A.data[i]
-
-# func argmax*[T; shape: static TensorShape](A: Tensor[T, shape]): int =
-#     result = 0
-#     var maxVal = A.data[0]
-#     for i in 1..<A.data.len:
-#         if A.data[i] > maxVal:
-#             maxVal = A.data[i]
-#             result = i
-
-# func argmin*[T; shape: static TensorShape](A: Tensor[T, shape]): int =
-#     result = 0
-#     var minVal = A.data[0]
-#     for i in 1..<A.data.len:
-#         if A.data[i] < minVal:
-#             minVal = A.data[i]
-#             result = i
-
 # ~~~~~~~~~~~~~~~~~~~~~
 # comparison operations
 # ~~~~~~~~~~~~~~~~~~~~~
@@ -209,35 +130,6 @@ func lessEqual*[T; n: static int](a, b: StridedVector[T, n], result: var Strided
     for i in 0..<n:
         result.data[i] = a.data[i] <= b.data[i]
 
-# func `==`*[T; shape: static TensorShape](A, B: Tensor[T, shape]): Tensor[bool, shape] =
-#     result = initTensor[bool, shape]()
-#     for i in 0..<result.data.len:
-#         result.data[i] = A.data[i] == B.data[i]
-
-# func `!=`*[T; shape: static TensorShape](A, B: Tensor[T, shape]): Tensor[bool, shape] =
-#     result = initTensor[bool, shape]()
-#     for i in 0..<result.data.len:
-#         result.data[i] = A.data[i] != B.data[i]
-
-# func `>`*[T; shape: static TensorShape](A, B: Tensor[T, shape]): Tensor[bool, shape] =
-#     result = initTensor[bool, shape]()
-#     for i in 0..<result.data.len:
-#         result.data[i] = A.data[i] > B.data[i]
-
-# func `>=`*[T; shape: static TensorShape](A, B: Tensor[T, shape]): Tensor[bool, shape] =
-#     result = initTensor[bool, shape]()
-#     for i in 0..<result.data.len:
-#         result.data[i] = A.data[i] >= B.data[i]
-
-# func `<`*[T; shape: static TensorShape](A, B: Tensor[T, shape]): Tensor[bool, shape] =
-#     result = initTensor[bool, shape]()
-#     for i in 0..<result.data.len:
-#         result.data[i] = A.data[i] < B.data[i]
-
-# func `<=`*[T; shape: static TensorShape](A, B: Tensor[T, shape]): Tensor[bool, shape] =
-#     result = initTensor[bool, shape]()
-#     for i in 0..<result.data.len:
-#         result.data[i] = A.data[i] <= B.data[i]
 
 # ~~~~~~~~~~~~~~~~~~~~~~~
 # miscellaneous functions
@@ -274,47 +166,7 @@ func sin*[T; n: static int](a: StridedVector[T, n], result: var StridedVector[T,
 
 func clamp*[T; n: static int](a: StridedVector[T, n], min_val, max_val: T, result: var StridedVector[T, n]) =
     for i in 0..<n:
-        result.data[i] = clamp(a.data[i], min_val .. max_val)
-
-# func recip*[T; shape: static TensorShape](A: Tensor[T, shape]): Tensor[T, shape] =
-#     result = initTensor[T, shape]()
-#     for i in 0..<result.data.len:
-#         result.data[i] = 1.T / A.data[i]
-
-# func abs*[T; shape: static TensorShape](A: Tensor[T, shape]): Tensor[T, shape] =
-#     result = initTensor[T, shape]()
-#     for i in 0..<result.data.len:
-#         result.data[i] = abs(A.data[i])
-
-# func sqrt*[T; shape: static TensorShape](A: Tensor[T, shape]): Tensor[T, shape] =
-#     result = initTensor[T, shape]()
-#     for i in 0..<result.data.len:
-#         result.data[i] = sqrt(A.data[i])
-
-# func sq*[T; shape: static TensorShape](A: Tensor[T, shape]): Tensor[T, shape] =
-#     result = initTensor[T, shape]()
-#     for i in 0..<result.data.len:
-#         result.data[i] = A.data[i] * A.data[i]
-
-# func ln*[T; shape: static TensorShape](A: Tensor[T, shape]): Tensor[T, shape] =
-#     result = initTensor[T, shape]()
-#     for i in 0..<result.data.len:
-#         result.data[i] = ln(A.data[i])
-
-# func exp*[T; shape: static TensorShape](A: Tensor[T, shape]): Tensor[T, shape] =
-#     result = initTensor[T, shape]()
-#     for i in 0..<result.data.len:
-#         result.data[i] = exp(A.data[i])
-
-# func sin*[T; shape: static TensorShape](A: Tensor[T, shape]): Tensor[T, shape] =
-#     result = initTensor[T, shape]()
-#     for i in 0..<result.data.len:
-#         result.data[i] = sin(A.data[i])
-
-# func clamp*[T; shape: static TensorShape](A: Tensor[T, shape], min_val, max_val: T): Tensor[T, shape] =
-#     result = initTensor[T, shape]()
-#     for i in 0..<result.data.len:
-#         result.data[i] = clamp(A.data[i], min_val .. max_val) # TODO: avoid slice?
+        result.data[i] = clamp(a.data[i], min_val .. max_val) # TODO: avoid slice?
 
 # ~~~~~~~~~~~~~~~~~~~~
 # activation functions
@@ -341,26 +193,6 @@ func tanh*[T; n: static int](a: StridedVector[T, n], result: var StridedVector[T
             expX = exp(x)
             expNegX = exp(-x)
         result.data[i] = (expX - expNegX) / (expX + expNegX)
-
-# func identity*[T; shape: static TensorShape](A: Tensor[T, shape]): Tensor[T, shape] =
-#     result = A
-
-# func relu*[T; shape: static TensorShape](A: Tensor[T, shape]): Tensor[T, shape] =
-#     result = initTensor[T, shape]()
-#     for i in 0..<result.data.len:
-#         result.data[i] = if A.data[i] > T(0): A.data[i] else: T(0)
-
-# func sigmoid*[T; shape: static TensorShape](A: Tensor[T, shape]): Tensor[T, shape] =
-#     result = recip(T(1) + exp(-A))
-
-# func tanh*[T; shape: static TensorShape](A: Tensor[T, shape]): Tensor[float, shape] =
-#     let
-#         negA = -A
-#         expA = exp(A)
-#         expNegA = exp(negA)
-#         numerator = expA - expNegA
-#         denominator = expA + expNegA
-#     result = hadamard(numerator, recip(denominator))
 
 # TODO:
 # Element-wise Leaky ReLu (?)
