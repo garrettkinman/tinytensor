9fff7bb43f5dbb181958f0a4583e14d2345c06ae
Garrett Kinman <garrettkinman@gmail.com>
2024-11-26 17:06:03 -0500

    matmul compile error relating to generics

--- src/tinytensor.nim
+++ src/tinytensor.nim
@@ -3,5 +3,5 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
-import tinytensor / [tensors, primops]
-export tensors, primops
+import tinytensor / [tensors, primops, matmul]
+export tensors, primops, matmul
